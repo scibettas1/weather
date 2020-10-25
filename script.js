9ea19aa7bd8a15138d4eb9d49c0911b5c2a1e7d6
Sam Scibetta <sammieeccentric@gmail.com>
2020-10-25 16:56:47 -0400

    the search bar is working. I need to re-think the buttons. the 5 day forcast is in progress

--- script.js
+++ script.js
@@ -1,33 +1,60 @@
+var searchForCity = function (city) {
+  //ajax call for current weather
+  $.ajax({
+    url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=ed721ed4c74684d8c960d41dea0e52e0",
+    method: "GET"
+  }).then(function (response) {
+    // convert Kelvin to farenheight
+    var temp = Math.ceil(((response.main.temp - 275.13) * 1.8) + 32);
+    $(".temp").html("Temp: " + temp + " &#8457;");
+    $(".city").html("<br>" + response.name + "<br>");
+    $(".wind").html("Wind: " + response.wind.speed + " MPH");
+    $(".humidity").html("Humidity: " + response.main.humidity + " &#37;");
+  });
+  //ajax call for 5 day forcast (in progress)
+  $.ajax({
+    url: "https://api.openweathermap.org/data/2.5/forcast?q=" + city + "&appid=ed721ed4c74684d8c960d41dea0e52e0",
+    method: "GET"
+  }).then(function (response) {
+    var temp = Math.ceil(((response.main.temp - 275.13) * 1.8) + 32);
+    $(".temp").html("Temp: " + temp + " &#8457;");
+    $(".city").html("<br>" + response.name + "<br>");
+    $(".wind").html("Wind: " + response.wind.speed + " MPH");
+    $(".humidity").html("Humidity: " + response.main.humidity + " &#37;");
+  });
+}
 
-var cityName = 'Wayne'
-
-//on click event with button
-$('#search-button').on("click", function () {
-  cityName = $("#search").val().trim()
+//on click event with buttons
+//this one is for the search bar
+$('#search-button').on("click", function (event) {
+  event.preventDefault()
+  var cityName = $("#search").val().trim();
+  console.log(cityName)
+  searchForCity(cityName)
 });
 
-$('#atlanta').on("click", function () {
+//look at our movie assignment and re-think these buttons
+/* $('#atlanta').on("click", function () {
   cityName = "Atlanta"
 });
-
-// Here we are building the URL we need to query the database
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=ed721ed4c74684d8c960d41dea0e52e0";
-
-
-// We then created an AJAX call
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function (response) {
-
-  // convert Kelvin to farenheight
-  var temp = Math.ceil(((response.main.temp - 275.13) * 1.8) + 32);
-  console.log(temp.toFixed(2))
-
-  $(".temp").html("Temperature: " + temp + " &#8457;");
-  $(".city").html("<br>City: " + response.name + "<br>");
-  $(".wind").html("Wind: " + response.wind.speed + " MPH");
-  $(".humidity").html("Humidity: " + response.main.humidity + " &#37;");
-
-
+$('#austin').on("click", function () {
+  cityName = "Austin"
 });
+$('#chicago').on("click", function () {
+  cityName = "Chicago"
+});
+$('#denver').on("click", function () {
+  cityName = "Denver"
+});
+$('#newYork').on("click", function () {
+  cityName = "New York"
+});
+$('#orlando').on("click", function () {
+  cityName = "Orlando"
+});
+$('#sanFrancisco').on("click", function () {
+  cityName = "San Francisco"
+});
+$('#seattle').on("click", function () {
+  cityName = "Seattle"
+}); */
