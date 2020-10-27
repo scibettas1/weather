369584bcd84e4ed67d6102d971ef0c6894ac5e6c
Sam Scibetta <sammieeccentric@gmail.com>
2020-10-27 18:33:57 -0400

    failed to push

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ var searchForCity = function (city) {
     $(".wind").html("Wind: " + response.wind.speed + " MPH");
     $(".humidity").html("Humidity: " + response.main.humidity + " &#37;");
   });
+
   //ajax call for 5 day forcast (in progress)
   $.ajax({
     url: "https://api.openweathermap.org/data/2.5/forcast?q=" + city + "&appid=ed721ed4c74684d8c960d41dea0e52e0",
@@ -29,11 +30,26 @@ var searchForCity = function (city) {
 $('#search-button').on("click", function (event) {
   event.preventDefault()
   var cityName = $("#search").val().trim();
-  console.log(cityName)
   searchForCity(cityName)
+  localStorage.setItem(cityName);
 });
 
+
+//save searches to local storage
+
+//for loop to run through array in local storage
+function pastSearch() {
+  for (var i = 0; i < cityName.length; i++) {
+    var button = $("<button>");
+    button.text(cityName[i]).appendTo("aside");
+  }
+}
+
+$(cityName).val(localStorage.getItem("9"));
+
+
 //look at our movie assignment and re-think these buttons
+
 /* $('#atlanta').on("click", function () {
   cityName = "Atlanta"
 });
