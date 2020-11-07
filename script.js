f9b08c8ac98fd4dd5d32a35b2b3c627571442d45
Sam Scibetta <sammieeccentric@gmail.com>
2020-11-07 09:35:19 -0500

    corrected 5 day forceasdt that broke

--- script.js
+++ script.js
@@ -42,33 +42,45 @@ $(document).ready(function () {
     }).then(function (response) {
       console.log(response)
       // for loop to loop over the forcast 5 times
-      for (var i = 0; i < 40; i++) {
-        if (response.list[i].dt_txt.indexOf('12:00:00') !== -1) {
+      //variable for an array the loops over anything at 12 pm
+      var all3HourForcasts = response.list
+      console.log(all3HourForcasts)
+      //tutor helped me with the filter method
+      var noonForcasts = all3HourForcasts.filter(function (forcast) {
+        if (forcast.dt_txt.indexOf('12:00:00') !== -1) {
+          return true
+        } else {
+          return false
+        }
+      })
+      console.log(noonForcasts)
+      for (var i = 0; i < noonForcasts.length; i++) {
+
           var integer = i
-          console.log(i)
+          //console.log(i)
           //weather variables
           var dateConvertVal = dateConvert();
-          console.log(dateConvertVal);
-          var temp2 = Math.floor((response.list[i].main.temp - 273.15) * 1.8 + 32) + " °F";
-          var icon2 = "<img src=https://openweathermap.org/img/wn/" + response.list[i].weather[0].icon + ".png>";
-          var humidity = (response.list[i].main.humidity) + "%";
+          //console.log(dateConvertVal);
+          var temp2 = Math.floor((noonForcasts[i].main.temp - 273.15) * 1.8 + 32) + " °F";
+          //console.log(temp2)
+          var icon2 = "<img src=https://openweathermap.org/img/wn/" + noonForcasts[i].weather[0].icon + ".png>";
+          var humidity = (noonForcasts[i].main.humidity) + "%";
           $("#date-" + integer).html(dateConvertVal);
           $("#icon-" + integer).html(icon2);
           $("#temp-" + integer).html("Temp: " + temp2);
           $("#humid-" + integer).html("Humidity: " + humidity);
           function dateConvert() {
-            var a = new Date(response.list[i].dt_txt);
-            console.log(a)
+            var a = new Date(noonForcasts[i].dt_txt);
+            //console.log(a)
             var monthList = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
             var year = a.getFullYear();
             var month = monthList[a.getMonth()];
             var date = a.getDate();
-            console.log(date)
+            //console.log(date)
             var time = month + ' ' + date + ', ' + year;
-            console.log(time)
+            //console.log(time)
             return time;
           }
-        }
       };
     });
   }
